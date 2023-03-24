72671ff698b392304016691c29aede09938f6903
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-24 16:18:33 +0100

    Initial Commit

---
+++ SwiftUIViewToPDF/SwiftUIViewToPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewToPDFApp.swift
+//  SwiftUIViewToPDF
+//
+//  Created by Kaori Persson on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewToPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
