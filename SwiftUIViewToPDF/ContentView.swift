b23cf275af4a08f770d885ce651a0b838e218417
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-24 16:21:46 +0100

    initial commit

--- SwiftUIViewToPDF/ContentView.swift
+++ SwiftUIViewToPDF/ContentView.swift
@@ -7,20 +7,54 @@
 
 import SwiftUI
 
+@MainActor
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ShareLink("Export PDF", item: render())
+  }
+  
+  func render() -> URL {
+    // 1: Render Hello World with some modifiers
+    let renderer = ImageRenderer(content:
+                                  Text("Hello, world!")
+      .font(.largeTitle)
+      .foregroundColor(.white)
+      .padding()
+      .background(.blue)
+      .clipShape(Capsule())
+    )
+    
+    // 2: Save it to our documents directory
+    let url = URL.documentsDirectory.appending(path: "output.pdf")
+    
+    // 3: Start the rendering process
+    renderer.render { size, context in
+      // 4: Tell SwiftUI our PDF should be the same size as the views we're rendering
+      var box = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+      
+      // 5: Create the CGContext for our PDF pages
+      guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else {
+        return
+      }
+      
+      // 6: Start a new PDF page
+      pdf.beginPDFPage(nil)
+      
+      // 7: Render the SwiftUI view data onto the page
+      context(pdf)
+      
+      // 8: End the page and close the file
+      pdf.endPDFPage()
+      pdf.closePDF()
     }
+    
+    return url
+  }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
